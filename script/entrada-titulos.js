2a5275b4a77957afcafb4e8b450ae8854d2118bb
Igor Calandrine <igorwow@hotmail.com>
2026-04-20 17:00:47 -0300

    classe entrada de títulos

---
+++ script/entrada-titulos.js
@@ -0,0 +1,38 @@
+/*Para o funcionamento é importante acrescentar data-titulos onde se deseja que se tenha uma entrada animada
+
+Pode-se modificar a posição de entrada ajustando a variável posição em #posicaoItemTela (posicao)*/ 
+export default class TitulosAnimados {
+   constructor (item) {
+      this.item = document.querySelectorAll(item)
+   }
+
+   alturaTelaTela () {
+      const alturaTela = window.innerHeight
+      return alturaTela
+   }
+
+   mostrarPrimeiroTitulo () {
+      this.item[0].dataset.titulos = "animados"
+   }
+
+   posicaoItemTela (posicao) {
+      window.addEventListener("scroll", () => {
+         this.item.forEach((item) => {
+            const topItem = item.getBoundingClientRect().top
+
+            if (topItem < this.alturaTelaTela () * posicao) {
+               item.dataset.titulos = "animados"
+            }
+            else if (topItem > this.alturaTelaTela()) {
+               item.dataset.titulos = ""
+            }
+         })
+      })
+   }
+
+   iniciar() {
+      this.mostrarPrimeiroTitulo()
+      this.posicaoItemTela(0.85)
+   }
+    
+}
--- script/script.js
+++ script/script.js
@@ -1,36 +1,39 @@
 import AbrirSubmenu from "./abrir-submenu.js"
 import MostrarTexto from "./mostrar-texto.js"
+import TitulosAnimados from "./entrada-titulos.js"
 
 const perguntas = new AbrirSubmenu(".pergunta")
 const textoAnimais = new MostrarTexto("[data-textos] div", "[data-animais] div")
+const titulosAnimados = new TitulosAnimados("[data-titulos]")
 
 perguntas.iniciar()
 textoAnimais.iniciar()
+titulosAnimados.iniciar()
 
-function TituloAnimadoScroll() {
-   const alturaTela = window.innerHeight
-   const tituloAnima = document.querySelectorAll(".js-scrollAnima")
+// function TituloAnimadoScroll() {
+//    const alturaTela = window.innerHeight
+//    const tituloAnima = document.querySelectorAll(".js-scrollAnima")
 
-   tituloAnima.forEach((item) => {
-      item.classList.add("js-scrollAnimação")
-   })
-   tituloAnima[0].classList.remove("js-scrollAnimação")
+//    tituloAnima.forEach((item) => {
+//       item.classList.add("js-scrollAnimação")
+//    })
+//    tituloAnima[0].classList.remove("js-scrollAnimação")
 
-   window.addEventListener("scroll", () => {
-      tituloAnima.forEach((item) => {
-         if (
-            item.getBoundingClientRect().bottom > 0 &&
-            item.getBoundingClientRect().top < alturaTela * 0.85
-         ) {
-            item.classList.remove("js-scrollAnimação")
-         } else if (
-            item.getBoundingClientRect().bottom < 0 ||
-            item.getBoundingClientRect().top > alturaTela * 0.7
-         ) {
-            item.classList.add("js-scrollAnimação")
-         }
-      })
-   })
-}
+//    window.addEventListener("scroll", () => {
+//       tituloAnima.forEach((item) => {
+//          if (
+//             item.getBoundingClientRect().bottom > 0 &&
+//             item.getBoundingClientRect().top < alturaTela * 0.85
+//          ) {
+//             item.classList.remove("js-scrollAnimação")
+//          } else if (
+//             item.getBoundingClientRect().bottom < 0 ||
+//             item.getBoundingClientRect().top > alturaTela * 0.7
+//          ) {
+//             item.classList.add("js-scrollAnimação")
+//          }
+//       })
+//    })
+// }
 
-TituloAnimadoScroll()
+// TituloAnimadoScroll()
